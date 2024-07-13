b392846d1fec67c1b0c5f4daeac1110231573f49
Tilly Persson <Devioxic@gmail.com>
2024-07-13 18:18:13 +0200

    Initial Commit

---
+++ Foxy/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Foxy
+//
+//  Created by Tilly Persson on 2024-07-13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Foxy/FoxyApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoxyApp.swift
+//  Foxy
+//
+//  Created by Tilly Persson on 2024-07-13.
+//
+
+import SwiftUI
+
+@main
+struct FoxyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
